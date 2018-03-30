eaa1938e1e1359d4dd3b1a0e39f9d27aaf3f3211
SunMeng <1658824587@qq.com>
2018-03-30 20:43:56 +0800

    新增数据库帮助类，以及各表对应的实体类

---
+++ src/model/City.java
@@ -0,0 +1,40 @@
+package model;
+
+public class City {
+	private int id;
+	private String CityName;
+	private String CityCode;
+	private int provinceId;
+	
+	public int getId(){
+		return id;
+	}
+	
+	public void setId(int id){
+		this.id=id;
+	}
+	
+	public String getCityName(){
+		return CityName;
+	}
+	
+	public void setCityName(String cityName){
+		this.CityName=cityName;
+	}
+	
+	public String getCityCode(){
+		return CityCode;
+	}
+	
+	public void setCityCode(String cityCode){
+		this.CityCode=cityCode;
+	}
+	public int getProvinceId() 
+	{ 
+		return provinceId; 
+		} 
+	public void setProvinceId(int provinceId) { 
+		this.provinceId = provinceId; 
+		}
+
+}
